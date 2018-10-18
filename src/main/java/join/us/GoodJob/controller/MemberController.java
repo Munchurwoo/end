afd068877ecd784e401e4c013647c1e010fe8bb0
miriSeo <mettuk00@gmail.com>
2018-10-18 11:02:22 +0900

    .

--- src/main/java/join/us/GoodJob/controller/MemberController.java
+++ src/main/java/join/us/GoodJob/controller/MemberController.java
@@ -7,6 +7,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
