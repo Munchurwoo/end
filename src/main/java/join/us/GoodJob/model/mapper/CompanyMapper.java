334e20e471f9e101a20fca9cacbbfe13e1091424
HSJ <hsj112211@naver.com>
2018-10-25 10:31:11 +0900

    ㅠ_ㅠ 다시 커밋 성진

--- src/main/java/join/us/GoodJob/model/mapper/CompanyMapper.java
+++ src/main/java/join/us/GoodJob/model/mapper/CompanyMapper.java
@@ -93,10 +93,6 @@ public interface CompanyMapper {
 	CompanyMemberVO jobPostingDetail(String jobPostingNum);
 	// 면접신청자 조회 ->
 	List<InterviewVO> getAllInterviewerList(String companyId);
-	//구인공고 키워드 등록	
-	// 헤더/채용정보 페이징처리
- 	// 면접신청자 조회 ->
- 	List<InterviewVO> getAllInterviewerList();
  	//구인공고 키워드 등록	
  	// 헤더/채용정보 페이징처리
 	int getAlljobPostingCount();
